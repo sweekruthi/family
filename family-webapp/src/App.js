83430eb8dc5774c3ae9f87f7824f79afc20ca119
Sweekruthi Raghunathan <raghus@uw.edu>
2020-08-16 19:54:08 -0700

    draw simple tree

--- family-webapp/src/App.js
+++ family-webapp/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Tree, treeUtil } from 'react-d3-tree';
+import { useState, useEffect } from 'react';
 
-function App() {
+function App(props) {
+
+  const jsonSource = 'file:///Users/sweekruthi/development/family/family-webapp/src/data.json';
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    treeUtil.parseJSON(jsonSource)
+            .then((data) => {
+              const wrapper = { data };
+              setInfo(wrapper);
+            })
+            .catch((err) => console.error(err));
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="treeWrapper" style={{width: '50em', height: '20em'}}>
+      <Tree data={info} />
     </div>
   );
 }
